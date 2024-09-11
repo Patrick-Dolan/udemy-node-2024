dec27667833bb6c2f042bc76c3aba429f2114e51
Patrick Dolan <dolanp1992@gmail.com>
2024-09-11 09:38:09 -0700

    show common request data and a simple response

--- 01-1-understanding-the-basics/app.js
+++ 01-1-understanding-the-basics/app.js
@@ -1,8 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
   // process.exit(); Can be used to exit the event loop and end the program.
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>')
+  res.write('<head><title>Server response</title></head>')
+  res.write(`<body><h1>${req.url}</h1></body>`)
+  res.write('</html>')
+  res.end();
 });
 
 server.listen(3000);
