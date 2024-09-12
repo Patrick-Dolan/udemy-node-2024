502bccd4318adda805a9c5da96350310738c9a86
Patrick Dolan <dolanp1992@gmail.com>
2024-09-12 14:37:49 -0700

    complete first objective of assignment

--- 01-1-assignment/app.js
+++ 01-1-assignment/app.js
@@ -1,7 +1,14 @@
 // Assignment objectives:
-// [ ] Spin up Node.js server on port 3000
+// [X] Spin up Node.js server on port 3000
 // [ ] Handle two routes: "/" and "/users"
 //     [ ] Return some greeting text on "/"
 //     [ ] Return a list of dummy users (e.g. <ul><li>User 1</li></ul>)
 // [ ] Add a form with a "username" <input> to the "/" page and submit a POST request to "/create-user" upon a button click
 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log("Server running.");
+})
+
+server.listen(3000);
