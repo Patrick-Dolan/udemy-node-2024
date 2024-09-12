ab68a33f629f06f6cc287067dd4f842fe745e21c
Patrick Dolan <dolanp1992@gmail.com>
2024-09-12 15:57:33 -0700

    add assignment objective I forgot

--- 01-1-assignment/app.js
+++ 01-1-assignment/app.js
@@ -4,6 +4,8 @@
 //     [X] Return some greeting text on "/"
 //     [X] Return a list of dummy users (e.g. <ul><li>User 1</li></ul>)
 // [ ] Add a form with a "username" <input> to the "/" page and submit a POST request to "/create-user" upon a button click
+// [ ] Add the "/create-user" route and parse the incoming data (i.e. the username) and simply log it to the console
+
 
 const http = require('http');
 const requestHandler = require('./routes');
