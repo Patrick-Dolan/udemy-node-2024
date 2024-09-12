49d13c5cd354c146f688dc11ea9043df7be5b91f
Patrick Dolan <dolanp1992@gmail.com>
2024-09-12 16:18:55 -0700

    add form submission for username to homepage

--- 01-1-assignment/routes.js
+++ 01-1-assignment/routes.js
@@ -1,11 +1,13 @@
 const requestHandler = (req, res) => {
   const url = req.url;
+  const method = req.method;
 
   if (url === "/") {
     res.write("<html>");
     res.write("<body>");
     res.write("<h1>Welcome to the assignment site!</h1>");
     res.write(`<a href="http://localhost:3000/users">Users</a>`);
+    res.write('<form action="/create-user" method="POST"><input name="username"><button>Send Username</button></form>');
     res.write("</body>");
     res.write("</html>");
     return res.end();
@@ -23,6 +25,13 @@ const requestHandler = (req, res) => {
     res.write(`<a href="http://localhost:3000/">Return home</a>`);
     res.write("</body>");
     res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/create-user" && method === "POST") {
+    console.log("User submitted");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
 
