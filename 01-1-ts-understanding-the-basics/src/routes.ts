cbb487e0315e65f48d3d0bec304a67ae307f1c2c
Patrick Dolan <dolanp1992@gmail.com>
2024-09-13 09:49:37 -0700

    add home and post route that creates message files

--- 01-1-ts-understanding-the-basics/src/routes.ts
+++ 01-1-ts-understanding-the-basics/src/routes.ts
@@ -1,7 +1,43 @@
 import { IncomingMessage, ServerResponse } from "http";
+import fs from "fs";
 
 export const requestHandler = (req: IncomingMessage, res: ServerResponse) => {
   const url = req.url;
+  const method = req.method;
+
+  // Homepage
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<h1>Typescript node server home</h1>");
+    res.write(
+      '<form action="/create-message" method="POST"><input name="message"><button>Send</button></form>'
+    );
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/create-message" && method === "POST") {
+    let body: Buffer[] = [];
+    req.on("data", (chunk: Buffer) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const messageText = parsedBody.split("=")[1];
+      fs.writeFile(
+        `./dist/message-${Math.random()}.txt`,
+        messageText,
+        (err) => {
+          if (err) throw err;
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          res.end();
+        }
+      );
+    });
+  }
 
   // Default route
   res.write("<html>");
