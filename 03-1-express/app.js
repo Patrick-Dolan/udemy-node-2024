53c867a5a2ac54a1f230169d2e844c0ece1b121e
Patrick Dolan <dolanp1992@gmail.com>
2024-09-16 11:48:22 -0700

    refactor app to use express and add comments to explain code

--- 03-1-express/app.js
+++ 03-1-express/app.js
@@ -1,6 +1,27 @@
-const http = require('http');
-const requestHandler = require("./routes");
+// Import express
+const express = require('express');
 
-const server = http.createServer(requestHandler);
+// Create app object by calling express
+const app = express();
 
-server.listen(3000);
+// The .use() method is what controls the middleware
+// The order and paths given to middleware matter
+// This first one will run every time no matter the path given
+// If you take off the next call it won't run the other paths since that tells the middleware to forward the request to the next one
+app.use('/', (req, res, next) => {
+  console.log("Always runs");
+  next();
+});
+
+// An example of a path, uses the .send method to give response
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>Add product!</h1>');
+});
+
+// This is the default that will run if no other path is matched
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+// This creates the server and listens on port given
+app.listen(3000);
