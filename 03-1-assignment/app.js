e5f122cc3b02dc82b81aa4c16936c33b41d2cad1
Patrick Dolan <dolanp1992@gmail.com>
2024-09-17 09:24:16 -0700

    add '/' and '/users' routes

--- 03-1-assignment/app.js
+++ 03-1-assignment/app.js
@@ -1,19 +1,17 @@
 // Assignment objectives
 // [X] 1. Create a npm project and install express.js (nodemon if you want)
 // [X] 2. Create an express.js app which funnels the requests through 2 middleware functions that log something to the console and return one response
-// [ ] 3. Handle requests "/" and "/users" such that each request only has on handler/middleware that does something with it (e.g. send dummy response)
+// [X] 3. Handle requests "/" and "/users" such that each request only has one handler/middleware that does something with it (e.g. send dummy response)
 
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Middleware 1");
-  next();
+app.use("/users", (req, res, next) => {
+  res.send("<h1>Users route.</h1>")
 });
 
-app.use((req, res, next) => {
-  console.log("Middleware 2");
+app.use("/", (req, res, next) => {
   res.send("<h1>Its a response from Express!</h1>")
 });
 
