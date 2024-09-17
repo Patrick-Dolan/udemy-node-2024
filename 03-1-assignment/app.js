538db4645e9f99f4cc01c9515140938ebafb2483
Patrick Dolan <dolanp1992@gmail.com>
2024-09-17 09:21:03 -0700

    add two middleware and a response for objective 2

--- 03-1-assignment/app.js
+++ 03-1-assignment/app.js
@@ -1,1 +1,20 @@
-console.log("Nodemon running.");
+// Assignment objectives
+// [X] 1. Create a npm project and install express.js (nodemon if you want)
+// [X] 2. Create an express.js app which funnels the requests through 2 middleware functions that log something to the console and return one response
+// [ ] 3. Handle requests "/" and "/users" such that each request only has on handler/middleware that does something with it (e.g. send dummy response)
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Middleware 1");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Middleware 2");
+  res.send("<h1>Its a response from Express!</h1>")
+});
+
+app.listen(3000);
