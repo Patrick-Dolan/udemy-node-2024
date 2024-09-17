b4ca7619c181d6403178f98004ec5a4c8a05907f
Patrick Dolan <dolanp1992@gmail.com>
2024-09-17 11:20:32 -0700

    add body-parser and post request example

--- 03-2-express/app.js
+++ 03-2-express/app.js
@@ -1,27 +1,21 @@
-// Import express
 const express = require('express');
+const bodyParser = require('body-parser');
 
-// Create app object by calling express
 const app = express();
 
-// The .use() method is what controls the middleware
-// The order and paths given to middleware matter
-// This first one will run every time no matter the path given
-// If you take off the next call it won't run the other paths since that tells the middleware to forward the request to the next one
-app.use('/', (req, res, next) => {
-  console.log("Always runs");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// An example of a path, uses the .send method to give response
 app.use('/add-product', (req, res, next) => {
-  res.send('<h1>Add product!</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
 });
 
-// This is the default that will run if no other path is matched
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express!</h1>');
 });
 
-// This creates the server and listens on port given
 app.listen(3000);
