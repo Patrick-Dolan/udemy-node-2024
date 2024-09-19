8042e62b33fd828febcf721a0bc4b4a4c19049c3
Patrick Dolan <dolanp1992@gmail.com>
2024-09-19 11:59:23 -0700

    add routes that serve HTML pages

--- 03-2-assignment/app.js
+++ 03-2-assignment/app.js
@@ -1,10 +1,18 @@
 // Assignment objectives
 // [X] Create a npm project and install Express.js (Nodemon too if you want)
-// [ ] Create an Express.js app which serves two HTML files for "/" and "/users"
+// [X] Create an Express.js app which serves two HTML files for "/" and "/users"
 // [ ] Add some static (.js or .css) files to your project that should be required by at least one of your HTML files. 
 
 const express = require('express');
+const path = require('path');
+const userRoutes = require('./routes/users');
 
 const app = express();
+
+app.use(userRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+});
 
 app.listen(3000);
