0a725c9762553e85ce6f62031a8ff0d8bc2beaaf
Patrick Dolan <dolanp1992@gmail.com>
2024-09-19 11:29:28 -0700

    create npm project with express and create server

---
+++ 03-2-assignment/app.js
@@ -0,0 +1,10 @@
+// Assignment objectives
+// [X] Create a npm project and install Express.js (Nodemon too if you want)
+// [ ] Create an Express.js app which serves two HTML files for "/" and "/users"
+// [ ] Add some static (.js or .css) files to your project that should be required by at least one of your HTML files. 
+
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
