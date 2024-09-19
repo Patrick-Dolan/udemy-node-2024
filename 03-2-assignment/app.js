fc0554aa47916f616921408abd8f9c1def910215
Patrick Dolan <dolanp1992@gmail.com>
2024-09-19 13:15:15 -0700

    add public folder and link script to homepage

--- 03-2-assignment/app.js
+++ 03-2-assignment/app.js
@@ -1,13 +1,15 @@
 // Assignment objectives
 // [X] Create a npm project and install Express.js (Nodemon too if you want)
 // [X] Create an Express.js app which serves two HTML files for "/" and "/users"
-// [ ] Add some static (.js or .css) files to your project that should be required by at least one of your HTML files. 
+// [X] Add some static (.js or .css) files to your project that should be required by at least one of your HTML files. 
 
 const express = require('express');
 const path = require('path');
 const userRoutes = require('./routes/users');
+const rootDir = require('./utils/rootDir');
 
 const app = express();
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use(userRoutes);
 
