d083f842a46fa15d2b8e218f5de3314447a71ba6
Patrick Dolan <dolanp1992@gmail.com>
2024-09-19 13:07:20 -0700

    add rootDir utility function and use it in routes

--- 03-2-assignment/routes/users.js
+++ 03-2-assignment/routes/users.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../utils/rootDir');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'homepage.html'));
+  res.sendFile(path.join(rootDir, 'views', 'homepage.html'));
 });
 
 router.get('/users', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'users.html'));
+  res.sendFile(path.join(rootDir, 'views', 'users.html'));
 });
 
 module.exports = router;
