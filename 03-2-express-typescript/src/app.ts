1b0b8d2b1d76eff0172b71a67261f6b82ffd8a53
Patrick Dolan <dolanp1992@gmail.com>
2024-09-20 08:25:16 -0700

    add body-parser deps and middleware

--- 03-2-express-typescript/src/app.ts
+++ 03-2-express-typescript/src/app.ts
@@ -1,6 +1,7 @@
 // Package imports
 import express from "express";
 import path from "path";
+import bodyParser from "body-parser";
 
 // Local imports
 import adminRoutes from "./routes/admin";
@@ -8,6 +9,8 @@ import shopRoutes from "./routes/shop";
 import srcDir from "./utils/srcDir";
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
