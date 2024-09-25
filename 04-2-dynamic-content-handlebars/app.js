0c685f2ed231df7024622341628a89f895a23592
Patrick Dolan <dolanp1992@gmail.com>
2024-09-25 15:33:15 -0700

    add handlebars and set it as view engine

--- 04-2-dynamic-content-handlebars/app.js
+++ 04-2-dynamic-content-handlebars/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
