f997ec31463338c0b7036ad15c1ff727ffe94aab
Patrick Dolan <dolanp1992@gmail.com>
2024-09-27 10:57:52 -0700

    refactor to use ejs for template engine

---
+++ 04-3-dynamic-content-ejs/routes/shop.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    activeShop: true,
+    productCSS: true,
+  });
+});
+
+module.exports = router;
