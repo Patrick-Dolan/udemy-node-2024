1f23f2d67e8b6487e52941175c571c4b3ef2aea2
Patrick Dolan <dolanp1992@gmail.com>
2024-09-27 09:39:20 -0700

    refactor project to use handlebars layouts

--- 04-2-dynamic-content-handlebars/routes/shop.js
+++ 04-2-dynamic-content-handlebars/routes/shop.js
@@ -5,7 +5,13 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
