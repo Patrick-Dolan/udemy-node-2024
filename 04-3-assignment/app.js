f45a28f6ff4019ef6561a31bd8e15a6fbd2c0d0e
Patrick Dolan <dolanp1992@gmail.com>
2024-10-03 09:46:00 -0700

    move app.js to root of project

---
+++ 04-3-assignment/app.js
@@ -0,0 +1,8 @@
+// Assignment objectives
+// [ ] Create a npm project and install Express.js, and a template engine of your choice.
+// Add two routes:
+//  [ ] "/" will hold a <form> that allows users to input their name
+//  [ ] "/users" outputs a <ul> with the user names (or some error text)
+// [ ] use layouts/partials in whatever template engine you use.
+
+console.log("Test")
