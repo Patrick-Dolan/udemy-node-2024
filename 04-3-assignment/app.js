959b3edaae6f9dccb16a6e2c75b0bc9bc3ca64b5
Patrick Dolan <dolanp1992@gmail.com>
2024-10-03 09:49:31 -0700

    finish first objective

--- 04-3-assignment/app.js
+++ 04-3-assignment/app.js
@@ -1,8 +1,6 @@
 // Assignment objectives
-// [ ] Create a npm project and install Express.js, and a template engine of your choice.
+// [X] Create a npm project and install Express.js, and a template engine of your choice.
 // Add two routes:
 //  [ ] "/" will hold a <form> that allows users to input their name
 //  [ ] "/users" outputs a <ul> with the user names (or some error text)
 // [ ] use layouts/partials in whatever template engine you use.
-
-console.log("Test")
