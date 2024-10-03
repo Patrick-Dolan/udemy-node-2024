2c0402918b82a2d6feceaceb4e8c6bedaff411de
Patrick Dolan <dolanp1992@gmail.com>
2024-10-03 14:08:42 -0700

    add users route and page

--- 04-3-assignment/routes/users.js
+++ 04-3-assignment/routes/users.js
@@ -7,6 +7,10 @@ router.get('/', (req, res) => {
   res.render('add-user', { docTitle: 'Add User' });
 });
 
+router.get('/users', (req, res) => {
+  res.render('users', { docTitle: 'Users' });
+});
+
 router.post('/add-user', (req, res) => {
   console.log('Post request reached for /add-user');
   res.redirect('/');
